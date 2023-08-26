8b1286a7711c0ff5d4f7f0c6119463613282ae6d
[AI6/1반] 김범진 <qjawls646@gmail.com>
2023-08-26 20:47:58 +0900

    section 9

---
+++ service/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { AccountsService } from './accounts.service';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  accounts: { name: string; status: string }[] = [];
+
+  constructor(private accountsService: AccountsService) {}
+
+  ngOnInit(): void {
+    this.accounts = this.accountsService.accounts;
+  }
+}
