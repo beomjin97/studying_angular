3857f7ab1b124fa4d3152e8db8eff0e8e745f333
[AI6/1반] 김범진 <qjawls646@gmail.com>
2023-09-20 17:47:16 +0900

    section 17

---
+++ learn-pipe/src/app/app.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { AppComponent } from './app.component';
+import { ShortenPipe } from './shorten.pipe';
+import { FilterPipe } from './filter.pipe';
+
+@NgModule({
+  declarations: [AppComponent, ShortenPipe, FilterPipe],
+  imports: [BrowserModule, FormsModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
